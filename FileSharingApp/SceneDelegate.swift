7a33ad9e565db91c21add59a5d6a981170be0a15
jollybangue <141067435+jollybangue@users.noreply.github.com>
2023-09-05 11:37:24 -0600

    Improved authentication feature
    - If the user is already logged in to the server, they will directly get the Home Screen when launching the app.
    
    - If the user is not yet logged in, they will get the Login Screen when launching the app.

--- FileSharingApp/SceneDelegate.swift
+++ FileSharingApp/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -16,7 +17,26 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        //guard let _ = (scene as? UIWindowScene) else { return }
+        
+        /// With the code below, if the user is already logged in to the server, they will directly get the Home Screen when launching the app
+        /// If the user is not yet logged in, they will get the Login Screen when launching the app.
+        Auth.auth().addStateDidChangeListener { _, currentUser in
+            if currentUser != nil {
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let navigationController = storyboard.instantiateInitialViewController() as? UINavigationController
+                let myRootViewController = storyboard.instantiateViewController(withIdentifier: "HomeVC") as UIViewController
+                navigationController?.viewControllers = [myRootViewController]
+                if let windowScene = scene as? UIWindowScene {
+                    let window = UIWindow(windowScene: windowScene)
+                    window.rootViewController = navigationController
+                    self.window = window
+                    window.makeKeyAndVisible()
+                }
+            }
+            print("The current logged in user is \(String(describing: currentUser))")
+        }
+
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
