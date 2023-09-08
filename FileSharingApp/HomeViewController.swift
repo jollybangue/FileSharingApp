d2637682d18969010c92ec9983e5ad71c32c6142
jollybangue <141067435+jollybangue@users.noreply.github.com>
2023-09-07 19:51:25 -0600

    Updated HomeViewController.swift
    Implemented a UITableView in Home view controller.

--- FileSharingApp/HomeViewController.swift
+++ FileSharingApp/HomeViewController.swift
@@ -12,14 +12,20 @@ import FirebaseAuth
 class HomeViewController: UIViewController {
     
     // Later, I can add a label in Home screen to show the email of the currently logged in user and set the text with the value of userEmail...
-        
+    
+    
+    @IBOutlet weak var homeTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        homeTableView.dataSource = self
+        
         /// Disable in the Navigation Controller the "Back to previous screen" button (located at the top left)
         navigationItem.setHidesBackButton(true, animated: false)
+        navigationController?.navigationBar.prefersLargeTitles = true
         
-        // I prefer not to show alerts here because it will also imply the management of if conditions
+        // I prefer NOT to show alerts here because it will also imply the management of if conditions
         // The alerts are directly triggered from Register or Login view controllers.
         
         guard let userEmail = Auth.auth().currentUser?.email else {
@@ -33,6 +39,15 @@ class HomeViewController: UIViewController {
     
     @IBAction func didTapUpload(_ sender: UIButton) {
     }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
     
     
     @IBAction func didTapSignOut(_ sender: UIButton) {
@@ -55,16 +70,24 @@ class HomeViewController: UIViewController {
         
         present(confirmationAlert, animated: true)
     }
+
+}
+
+extension HomeViewController: UITableViewDataSource {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //To be tested: let cell = UITableViewCell(style: .default, reuseIdentifier: "fileCell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "fileCell", for: indexPath)
+        cell.textLabel?.text = "Cloud file..."
+        return cell
+    }
 
+}
+
+extension HomeViewController: UITableViewDelegate {
+    
 }
