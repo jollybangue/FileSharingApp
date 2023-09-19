bd617bad0a7de03f4182699f20f4148bc7b1d987
jollybangue <141067435+jollybangue@users.noreply.github.com>
2023-09-19 11:19:09 -0600

    Update HomeViewController.swift
    Implemented the file (permanent) deletion feature, including all the different confirmation alerts.

--- FileSharingApp/HomeViewController.swift
+++ FileSharingApp/HomeViewController.swift
@@ -13,13 +13,13 @@ import FirebaseDatabase
 
 class HomeViewController: UIViewController {
     
-    // Later, I can add a label in Home screen to show the email of the currently logged in user and set the text with the value of userEmail...
+    // TODO: Later, I can add a label in Home screen to show the email of the currently logged in user and set the text with the value of userEmail...
     
     @IBOutlet weak var homeTableView: UITableView!
     
-    private let myStorageRef = Storage.storage().reference()
-    private let realtimeDBRef = Database.database().reference()
-    private let parentNode = "Files"
+    private let myStorageRef = Storage.storage().reference()    /// Firebase Cloud Storage refence
+    private let realtimeDBRef = Database.database().reference() /// Realtime database reference
+    private let rTDBparentNode = "Files" /// Root of the project in the Realtime database
     
     // USELESS: private var fileList: [StorageReference] = [] // Array containing the references (names, paths, links) of the files stored in the Firebase cloud storage.
     // var folderList: [StorageReference] = [] /// Array containing the references (names, paths, links) of the folders stored in the cloud.
@@ -45,6 +45,7 @@ class HomeViewController: UIViewController {
         getFileNamesFromRealtimeDB()
     }
     
+    /// This function get the list of files stored in the Firebase cloud storage and save it into the Realtime database using the setFileNamesInRealtimeDB() function.
     private func copyDataFromStorageToRealtimeDB() {
         myStorageRef.listAll { [self] result, error in
             if let unWrappedError = error {
@@ -53,21 +54,22 @@ class HomeViewController: UIViewController {
             // guard let myPrefixes = result?.prefixes else {return} // Array of folder references
             guard let fileReferences = result?.items else {return} // Array of file references
             setFileNamesInRealtimeDB(myFileList: fileReferences)
-            //fileList = fileReferences ///This line is useless. It is here just for debugging purposes.
         }
         // fileList is EMPTY here (outside the listAll function)
     }
     
+    /// This function cannot be directly called by the app, but ONLY by the function copyDataFromStorageToRealtimeDB() and can be ONLY used INSIDE the "listAll" function.
     private func setFileNamesInRealtimeDB(myFileList: [StorageReference]) {
-        realtimeDBRef.child(parentNode).removeValue() ///Deletes all the current values in realtime database to avoid duplication issues.
+        realtimeDBRef.child(rTDBparentNode).removeValue() ///Deletes all the current values in realtime database to avoid duplication issues.
         for item in myFileList {
             ///childbyAutoId() generates a random unique key associated with each file name.
-            realtimeDBRef.child(parentNode).childByAutoId().setValue(item.name)
+            realtimeDBRef.child(rTDBparentNode).childByAutoId().setValue(item.name)
         }
     }
     
+    /// This function allows the app to get and observe in realtime, the name of the files stored in the Firebase cloud storage.
     private func getFileNamesFromRealtimeDB() {
-        realtimeDBRef.child(parentNode).observe(.value) { [self] fileListSnapshot in
+        realtimeDBRef.child(rTDBparentNode).observe(.value) { [self] fileListSnapshot in
             guard let currentFileList = fileListSnapshot.value as? [String: String] else {return}
             realtimeFileList.removeAll() ///Deletes all values stored in realtimeFileList to avoid duplication issues.
             let sortedFileList = currentFileList.sorted{$0.0 < $1.0} ///Sorts by order the items stored in the currentFileList dictionnary and put them into sortedFileList
@@ -77,6 +79,19 @@ class HomeViewController: UIViewController {
             homeTableView.reloadData()
         }
         // realtimeFileList is EMPTY here (outside the observe() function)
+    }
+    
+    /// This function delete permanently from the Firebase Cloud Storage a file given its name.
+    private func deleteFileFromCloudStorage(nameOfTheFile: String) {
+        myStorageRef.child(nameOfTheFile).delete { [self] error in
+            if let fileDeletionError = error {
+                AlertManager.showAlert(myTitle: "File deletion error", myMessage: "Something went wrong while deleting the file \(nameOfTheFile).\nError: \(fileDeletionError)")
+            } else {
+                copyDataFromStorageToRealtimeDB() ///Getting the list of files available in the Firebase Cloud Storage and storing them in the realtime database.
+                getFileNamesFromRealtimeDB()
+                AlertManager.showAlert(myTitle: "File deleted", myMessage: "The file \(nameOfTheFile) has been succesfully deleted")
+            }
+        }
     }
 
     
@@ -133,9 +148,12 @@ extension HomeViewController: UITableViewDelegate {
         
         tableView.deselectRow(at: indexPath, animated: true)
         
-        let fileRef = myStorageRef.child(realtimeFileList[indexPath.row].1)
+        let fileSelected: String = realtimeFileList[indexPath.row].1
         
-        fileRef.getMetadata { [self] metadata, error in
+        // TODO: In the future, for optimization, I should implement one function to get all the metadata (including the list of files) from Firebase cloud storage and save them in the Realtime database.
+        /// And all the information needed by the application should be get from the Realtime database.
+        /// It is better to proceed like that because is not possible to access informations like metadata or file list outside the Cloud Storage functions getMetadata and listAll.
+        myStorageRef.child(fileSelected).getMetadata { [self] metadata, error in
             if let myError = error {
                 AlertManager.showAlert(myTitle: "Error", myMessage: "Something went wrong with metadata. Error \(myError)")
             }
@@ -145,7 +163,7 @@ extension HomeViewController: UITableViewDelegate {
                   let filetimeModified = metadata?.updated?.formatted(date: .abbreviated, time: .standard),
                   let fileName = metadata?.name
             else {return}
-            let fileDetailsAlert = UIAlertController(title: realtimeFileList[indexPath.row].1, message: "\nKind: \(fileKind) file\n" + "\nSize: \(fileSize) bytes\n" + "\nCreated: \(fileTimeCreated)\n" + "\nModified: \(filetimeModified)\n", preferredStyle: .alert)
+            let fileDetailsAlert = UIAlertController(title: fileSelected, message: "\nKind: \(fileKind) file\n" + "\nSize: \(fileSize) bytes\n" + "\nCreated: \(fileTimeCreated)\n" + "\nModified: \(filetimeModified)\n", preferredStyle: .alert)
             
             let openFileAction = UIAlertAction(title: "Open", style: .default) { _ in
             }
@@ -157,17 +175,26 @@ extension HomeViewController: UITableViewDelegate {
                 AlertManager.showAlert(myTitle: fileName, myMessage: "File link copied to clipboard")
             }
             
-            let deleteFileAction = UIAlertAction(title: "Delete", style: .destructive) { _ in
+            let deleteFileAction = UIAlertAction(title: "Delete", style: .destructive) { [self] _ in
                 
+                let deleteConfirmationAlert = UIAlertController(title: "Delete File", message: "Do you want to permanently delete the file \(fileSelected) from the cloud?", preferredStyle: .alert)
+                
+                let deleFileAction = UIAlertAction(title: "Delete", style: .destructive) { [self] _ in
+                    deleteFileFromCloudStorage(nameOfTheFile: fileSelected) /// Delete permanently the file named "fileToBeDeleted" from the Firebase Cloud Storage.
+                    // TODO: For future improvement, instead of permanently delete the files from the Firebase cloud storage, the files should just be moved to a trash or recycle bin.
+                }
+                
+                deleteConfirmationAlert.addAction(deleFileAction)
+                deleteConfirmationAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+                
+                present(deleteConfirmationAlert, animated: true)
             }
-            
-            let cancelFileAction = UIAlertAction(title: "Cancel", style: .cancel)
-            
+                        
             fileDetailsAlert.addAction(openFileAction)
             fileDetailsAlert.addAction(downloadFileAction)
             fileDetailsAlert.addAction(shareFileAction)
             fileDetailsAlert.addAction(deleteFileAction)
-            fileDetailsAlert.addAction(cancelFileAction)
+            fileDetailsAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
             
             present(fileDetailsAlert, animated: true)
         }
@@ -175,14 +202,23 @@ extension HomeViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         
-        let deleteFileContextualAction = UIContextualAction(style: .destructive, title: "Delete") { _,_,_ in
-            
+        let fileToBeDeleted: String = realtimeFileList[indexPath.row].1
+        
+        let deleteConfirmationAlert = UIAlertController(title: "Delete File", message: "Do you want to permanently delete the file \(fileToBeDeleted) from the cloud?", preferredStyle: .alert)
+        
+        let deleFileAction = UIAlertAction(title: "Delete", style: .destructive) { [self] _ in
+            deleteFileFromCloudStorage(nameOfTheFile: fileToBeDeleted) /// Delete permanently the file named "fileToBeDeleted" from the Firebase Cloud Storage.
+            // TODO: For future improvement, instead of permanently delete the files from the Firebase cloud storage, the files should just be moved to a trash or recycle bin.
         }
         
-        let deleteFileConfiguration = UISwipeActionsConfiguration(actions: [deleteFileContextualAction])
+        deleteConfirmationAlert.addAction(deleFileAction)
+        deleteConfirmationAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         
+        let deleteFileContextualAction = UIContextualAction(style: .destructive, title: "Delete") { [self] action, view, completion in
+            present(deleteConfirmationAlert, animated: true)
+            completion(true)
+        }
         
-        
-        return deleteFileConfiguration
+        return UISwipeActionsConfiguration(actions: [deleteFileContextualAction])
     }
 }
